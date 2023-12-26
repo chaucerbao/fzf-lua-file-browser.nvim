034c3861ddf2e3567ea09b99e05f5ea30ccb9dfd
Alvin Chan <aschan@gmail.com>
2023-12-25 23:48:13 -0700

    Initial commit

---
+++ lua/fzf-lua-file-browser/actions.lua
@@ -0,0 +1,158 @@
+-- Imports
+local actions = require('fzf-lua.actions')
+local path = require('fzf-lua.path')
+local utils = require('fzf-lua-file-browser.utils')
+
+-- Helpers
+local entry_to_fullpath = utils.entry_to_fullpath
+local input = utils.input
+
+-- Actions
+local create = {
+  function(selected, opts)
+    local fullpath = entry_to_fullpath(opts.cwd or vim.loop.cwd(), opts)
+    local relpath = path.HOME_to_tilde(fullpath)
+
+    local target_file =
+      input('Create: ' .. relpath .. (path.ends_with_separator(relpath) and '' or path.separator()), nil, 'file')
+    if not target_file or #target_file == 0 then
+      return false
+    end
+
+    local fullpath_to_target_file = entry_to_fullpath(target_file, opts)
+    if vim.fn.filereadable(fullpath_to_target_file) > 0 then
+      require('fzf_lua.utils').err('Already exists!')
+      return false
+    end
+
+    if path.ends_with_separator(fullpath_to_target_file) then
+      vim.loop.fs_mkdir(fullpath_to_target_file, 493) -- 0x755
+    else
+      local file_handler = vim.loop.fs_open(fullpath_to_target_file, 'w', 420) -- 0x644
+      if file_handler then
+        vim.loop.fs_close(file_handler)
+      end
+    end
+
+    return true
+  end,
+  actions.resume,
+}
+
+local rename = {
+  function(selected, opts)
+    if #selected == 0 then
+      return false
+    end
+
+    local fullpath_to_file = entry_to_fullpath(selected[1], opts)
+    local relpath_to_file = path.HOME_to_tilde(fullpath_to_file)
+
+    local target_file = input('Rename to: ', path.basename(relpath_to_file), 'file')
+    if not target_file or #target_file == 0 then
+      return false
+    end
+
+    local fullpath_to_target_file = entry_to_fullpath(target_file, opts)
+    if vim.fn.filereadable(fullpath_to_target_file) == 0 or (input('Overwrite?' .. ' [y/n] ') == 'y') then
+      vim.loop.fs_rename(fullpath_to_file, fullpath_to_target_file)
+    end
+
+    return true
+  end,
+  actions.resume,
+}
+
+local copy = {
+  function(selected, opts)
+    if #selected == 0 then
+      return false
+    end
+
+    local fullpath_to_file = entry_to_fullpath(selected[1], opts)
+    local relpath_to_file = path.HOME_to_tilde(fullpath_to_file)
+
+    local target_file = input('Copy to: ', relpath_to_file, 'file')
+    if not target_file or #target_file == 0 then
+      return false
+    end
+
+    local fullpath_to_target_file = entry_to_fullpath(target_file, opts)
+    if vim.fn.filereadable(fullpath_to_target_file) == 0 or (input('Overwrite?' .. ' [y/n] ') == 'y') then
+      vim.loop.fs_copyfile(fullpath_to_file, fullpath_to_target_file)
+    end
+
+    return true
+  end,
+  actions.resume,
+}
+
+local move = {
+  function(selected, opts)
+    if #selected == 0 then
+      return false
+    end
+
+    local fullpath_to_file = entry_to_fullpath(selected[1], opts)
+    local relpath_to_file = path.HOME_to_tilde(fullpath_to_file)
+
+    local target_file = input('Move to: ', relpath_to_file, 'file')
+    if not target_file or #target_file == 0 then
+      return false
+    end
+
+    local fullpath_to_target_file = entry_to_fullpath(target_file, opts)
+    if vim.fn.filereadable(fullpath_to_target_file) == 0 or (input('Overwrite?' .. ' [y/n] ') == 'y') then
+      if vim.loop.fs_copyfile(fullpath_to_file, fullpath_to_target_file) then
+        vim.loop.fs_unlink(fullpath_to_file)
+      end
+    end
+
+    return true
+  end,
+  actions.resume,
+}
+
+local delete = {
+  function(selected, opts)
+    if #selected == 0 then
+      return false
+    end
+
+    local msg
+    if #selected > 1 then
+      msg = 'Delete ' .. #selected .. ' entries?'
+    else
+      local fullpath_to_file = entry_to_fullpath(selected[1], opts)
+      msg = 'Delete "'
+        .. path.basename(fullpath_to_file)
+        .. (path.ends_with_separator(fullpath_to_file) and path.separator() or '')
+        .. '"?'
+    end
+
+    if input(msg .. ' [y/n] ') ~= 'y' then
+      return false
+    end
+
+    for _, entry in ipairs(selected) do
+      local fullpath_to_file = entry_to_fullpath(entry, opts)
+
+      if vim.fn.isdirectory(fullpath_to_file) > 0 then
+        vim.loop.fs_rmdir(fullpath_to_file)
+      elseif vim.fn.filereadable(fullpath_to_file) > 0 then
+        vim.loop.fs_unlink(fullpath_to_file)
+      end
+    end
+
+    return true
+  end,
+  actions.resume,
+}
+
+return {
+  create = create,
+  rename = rename,
+  copy = copy,
+  move = move,
+  delete = delete,
+}
---
+++ lua/fzf-lua-file-browser/init.lua
@@ -0,0 +1,59 @@
+-- Imports
+local fzf_lua = require('fzf-lua')
+local path = fzf_lua.path
+local actions = require('fzf-lua-file-browser.actions')
+local utils = require('fzf-lua-file-browser.utils')
+
+-- Helpers
+local entry_to_fullpath = utils.entry_to_fullpath
+
+local function browse(opts)
+  opts = opts or {}
+
+  if opts.prompt == nil then
+    opts.prompt = 'Browse❯ '
+  end
+
+  opts.fn_transform = function(entry)
+    local fullpath = entry_to_fullpath(entry, opts)
+
+    if vim.fn.isdirectory(fullpath) > 0 then
+      entry = fzf_lua.utils.ansi_codes.magenta(entry) .. '/'
+    end
+
+    return fzf_lua.make_entry.file(entry, { file_icons = true, color_icons = true })
+  end
+
+  opts.actions = {
+    ['default'] = function(selected)
+      local fullpath = entry_to_fullpath(selected[1], opts)
+
+      if vim.fn.isdirectory(fullpath) > 0 then
+        browse(vim.tbl_deep_extend('force', opts, { cwd = fullpath }))
+      elseif vim.fn.filereadable(fullpath) > 0 then
+        fzf_lua.actions.file_edit(selected, opts)
+      end
+    end,
+
+    ['ctrl-g'] = function()
+      local fullpath = entry_to_fullpath(opts.cwd or vim.loop.cwd(), opts)
+      local parent = path.parent(fullpath)
+
+      if vim.fn.isdirectory(parent) > 0 then
+        browse(vim.tbl_deep_extend('force', opts, { cwd = parent }))
+      else
+        fzf_lua.actions.resume()
+      end
+    end,
+
+    ['alt-c'] = actions.create,
+    ['alt-r'] = actions.rename,
+    ['alt-y'] = actions.copy,
+    ['alt-m'] = actions.move,
+    ['alt-d'] = actions.delete,
+  }
+
+  return fzf_lua.fzf_exec('ls', opts)
+end
+
+return { browse_files = browse, actions = actions }
